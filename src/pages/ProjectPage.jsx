d8fa7fa452bc532ceee1f3059cf26c9cc160aeaf
Werosh Kriyanjala <weroshprofy@gmail.com>
2025-07-27 01:19:03 +0530

    adding real projects

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -16,20 +16,21 @@ import RubiImg from "../assets/project_images/rubi.webp";
 import RuhunuImg from "../assets/project_images/ruhunuprimeland.webp";
 import VisaImg from "../assets/project_images/vizalk.webp";
 import LucianImg from "../assets/project_images/lucian.webp";
+import DigasiriImg from "../assets/project_images/digasiri.webp";
 
 const categories = [
   "All Projects",
   "Website Development",
-  "SEO Development",
+  "Photography and Videography",
+  "AI Video Creations",
   "Digital Marketing",
+  "SEO Development",
   "Email Marketing",
   "Event Planning",
   "Software And Mobile Application Development",
   "Business Consultant",
-  "Photography and Videography",
   "Content Writing",
   "Content Creation",
-  "AI Video Creations",
   "Short Video Creations",
   "Content Planning and Copywriting",
   "Logo Design",
@@ -120,6 +121,160 @@ const projects = [
     client: "Ruhunu Trust & Credit (Pvt) Ltd",
   },
 
+  // Photography
+
+  {
+    id: 9,
+    title: "Chathu Rathnayaka Photography",
+    description:
+      "A heartfelt photo session capturing the pride, resilience, and achievement of newly graduated nurses. This shoot blends elegance with emotion—featuring crisp white uniforms, graduation gowns, stethoscopes, and natural smiles that reflect years of hard work and compassion. Set in serene outdoor backdrops or clean studio light, every frame tells a story of dedication, care, and a future ready to heal the world.",
+    image:
+      "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/504177350_705067985229125_4891690034899797545_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeECztu7o2fCQwcKrrXMBc_NxMXhP9LB2J3ExeE_0sHYnWfiLV6kvU7vS_RjYqoaLSmQ4KJ8oB_r6x5JfvFyMo2v&_nc_ohc=b7c3JXQylO4Q7kNvwEgITBG&_nc_oc=AdnOlIXPZY9z0vNx2SEhLOfT7Lhidi7Q8EvFO4xpqMIwxZ1FbRvUQnB3aS-DJ2Ir6-wYhRYt10v9RnurQgWDv6EU&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=4SKegBkktg-HpPMzE0zTJw&oh=00_AfTNZGrUJOLyLLrazr0SQ4Hl4y7ZmgvUWBNrdkaGSEdLCw&oe=688AFE56",
+    category: "Photography and Videography",
+    tags: ["Photography", "Product Shots", "Editing", "Commercial"],
+    demoUrl: "https://www.facebook.com/share/p/19kcr2aXgB/",
+    githubUrl: "",
+    date: "2025-06-05",
+    client: "Chathu Rathnayaka",
+  },
+  {
+    id: 10,
+    title: "Umesha Sidangana  Birth Day Photography",
+    description:
+      "A joyful and elegant birthday shoot capturing the personality and celebration of Umesha Sidangana’s special day. This session blends candid smiles, posed portraits, and detail shots of décor, cake, and style—framed with natural lighting and soft tones to create timeless memories full of charm and warmth.",
+    image:
+      "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/481947696_1226802818868380_250123787957236774_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeG9h5OZadq_vO7adzyVzPMxphMJIGX_MW2mEwkgZf8xbUgwne6ypNyRlI0LtKx0Vh_zHqQxa9WV8FjWWFnbVeLN&_nc_ohc=e-Rakh61thQQ7kNvwGIrxMh&_nc_oc=AdnDbMuvGMF6V4jnZHuFsCIx_hnfxQtCPaaCkT49I1kguaUtV3N8RkdRGv52pSyt_fq0EpvPMAvrXVfrWwxpopaE&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=4Kt5l_1D-uvjfAZDp1rjww&oh=00_AfTRMWTRXIOomhvCQddbdVCFopuTm72Q8_NX3K4R0XvbVw&oe=688B167C",
+    category: "Photography and Videography",
+    tags: ["Photography", "Product Shots", "Editing", "Commercial"],
+    demoUrl: "https://www.facebook.com/share/p/1LcUjMXycg/",
+    githubUrl: "",
+    date: "2025-01-01",
+    client: "Umesha Sidangana",
+  },
+  {
+    id: 11,
+    title: "Wes Mangalya Video Shoot",
+    description:
+      "A vibrant and cinematic video production capturing the elegance and rhythm of the Wes Mangalya, a traditional Sri Lankan dance ceremony performed during weddings. This shoot highlights graceful Kandyan dance movements, cultural attire, ceremonial drumming, and the spiritual energy that honors heritage, blessings, and unity. Shot with dynamic angles, slow-motion, and storytelling flow to reflect both the cultural depth and joyous celebration.",
+    image:
+      "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/487511216_590498024010094_1893953343173234933_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF_zWOpXhEKBtdcoc_5dym1niTgDsyuHQqeJOAOzK4dCn6ouEHME-_lEf_bHsU9ZSmFInOFMmZo6zCi2quF3gvU&_nc_ohc=FdjeLX2ed8UQ7kNvwFaVuOh&_nc_oc=Adlrow41HNMOdwle_wX40NV2CXxrylkCVH3trprUvSoe6Vh8x8Gdxw-QRry_XS4_lneljddd_OOey5D7_QhZxfrv&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=TZm38OjMI7WU_zIHlazIYA&oh=00_AfSUKltYvXYYgz9Z4GalDXVnwZVOEBAyIxBnrRrq-qOxiA&oe=688ADD38",
+    category: "Photography and Videography",
+    tags: [
+      "Photography",
+      "Cinematography",
+      "Videography",
+      "Editing",
+      "Commercial",
+    ],
+    demoUrl: "https://www.facebook.com/share/v/1ByQBipFfV/",
+    githubUrl: "",
+    date: "2025-02-25",
+    client: "Kumbukgete Central College",
+  },
+  {
+    id: 12,
+    title: "Couple Pre-Shoot – Love in Every Frame",
+    description:
+      "A dreamy outdoor pre-shoot capturing the chemistry, laughter, and tender moments of a couple before their big day. With a blend of candid poses, natural lighting, and scenic backdrops, this session tells a heartfelt visual story of love, connection, and new beginnings—framed with elegance and emotion.",
+    image:
+      "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/470216409_122096671952696103_6341353054595559678_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGlEGEL8t83ifZ3mD84c3uDWBjebRWosnlYGN5tFaiyeU6IYw6STuFujn4b-wTlWzumWLCLlXlQ4jdZ8pexvdLG&_nc_ohc=DugGuHQMOSsQ7kNvwEU3lF9&_nc_oc=Adk2InuQxG94idE9nT_Gc2hNvKxMNd3qUPCzv6xdjxl7KCB-cnFIQNv5BNUZg1GwajQZs4y-C3tsNVkAGsJdHZP9&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=c8BLKv6OtzaOyA20PDvv8A&oh=00_AfQyYGfFp-4_W-u9euzS3IEbQD9sVgcIhJuw9PG3Uo0_iw&oe=688AEF78",
+    category: "Photography and Videography",
+    tags: [
+      "Photography",
+      "Cinematography",
+      "Videography",
+      "Editing",
+      "Commercial",
+    ],
+    demoUrl: "https://www.facebook.com/share/v/1C1rfXNwdd/",
+    githubUrl: "",
+    date: "2025-02-25",
+    client: "Mr.Ravindu Bandara and Ms. Semini Sashiprapbha ",
+  },
+  {
+    id: 13,
+    title: "LandCruiser – Speed Ramp Cinematic Clip",
+    description:
+      "A high-intensity automotive video capturing the raw power and elegance of the Toyota LandCruiser in motion. Featuring dramatic speed ramping, cinematic angles, and dynamic sound design, this shoot highlights the vehicle's strength, control, and presence—ideal for commercial use, brand promotion, or social media campaigns.",
+    image:
+      "https://i.pinimg.com/736x/3e/46/db/3e46db1b1df92c5b89042df3df59c0f8.jpg",
+    category: "Photography and Videography",
+    tags: [
+      "Photography",
+      "Cinematography",
+      "Videography",
+      "Editing",
+      "Commercial",
+    ],
+    demoUrl:
+      "https://drive.google.com/file/d/1RAdjRyvslAMulRib_-smR48G4ObNDeRO/view?usp=drivesdk",
+    githubUrl: "",
+    date: "2024-08-25",
+    client: "Mr.Malshan Maduranga",
+  },
+  {
+    id: 14,
+    title: "Couple Pre-Shoot – Photography",
+    description:
+      "A romantic photography session celebrating the bond between two souls ahead of their wedding day. Blending natural light, candid expressions, and scenic settings, this shoot beautifully frames the joy, intimacy, and anticipation of their journey together. Every photo is a lasting memory crafted with emotion and style.",
+    image:
+      "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/470229579_122096673098696103_6183628911443746534_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeECsqlCw2pm3ZvGLM4wsn5EHimKjarwu9EeKYqNqvC70fDAagP77L_l1eWtO6CCWqHmcGbJZxnByaZXiB61Eoc8&_nc_ohc=ol99KIRdC8kQ7kNvwGAyplo&_nc_oc=Admf-UrTZ5NbD49QE-T-soJ5R6-3KJzMMv1Lo1TqEqQ7IztEoY_K7H9WB3Rpt8bkAYRO6JNPzRpyc9ohGf9_7igC&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=npzucgFifYv974TgAIliCQ&oh=00_AfQQx-5w6PtsAzWsuA8vJt39UHB8KTjwq9qQMKMwDUjslg&oe=688B06B6",
+    category: "Photography and Videography",
+    tags: ["Photography", "Product Shots", "Editing", "Commercial"],
+    demoUrl: "https://www.facebook.com/share/p/16krrAUwr7/",
+    githubUrl: "",
+    date: "2025-06-05",
+    client: "Lumeo Creations",
+  },
+
+  // AI Video Creation
+
+  //
+  {
+    id: 15,
+    title: "Diagnostic Lab Promo – AI-Generated Medical Video",
+    description:
+      "A modern, AI-generated promotional video crafted to build trust and awareness for a diagnostic laboratory. Featuring realistic visuals, professional narration, and informative content, the video highlights lab services, test reliability, and health benefits—delivered with a clean, medical-grade aesthetic perfect for digital marketing, clinics, and social media outreach.",
+    image: DigasiriImg,
+    category: "AI Video Creations",
+    tags: ["AI", "Video Generation", "Marketing", "Automation"],
+    demoUrl: "https://www.facebook.com/share/v/1EC9Co1n1G/",
+    githubUrl: "",
+    date: "2025-07-19",
+    client: "Digasiri Lab & Healthcare Pvt. Ltd ",
+  },
+
+  // Digital Marketing
+
+  {
+    id: 4,
+    title: "Vacanza in Ceylon – Digital Marketing Campaign",
+    description:
+      "A strategic digital marketing initiative designed to attract European travelers to Sri Lanka through targeted content, SEO, and social media engagement. The campaign highlights cultural tours, luxury stays, and authentic local experiences—driven by creative storytelling, performance tracking, and high-conversion design across platforms.",
+    image:
+      "https://www.pngitem.com/pimgs/m/510-5109369_digital-marketing-company-digital-marketing-banner-png-transparent.png",
+    category: "Digital Marketing",
+    tags: ["Facebook Ads", "Instagram", "Analytics", "Automation"],
+    demoUrl: "https://www.facebook.com/profile.php?id=61573583293914",
+
+    date: "2025-01-01",
+    client: "Vacanza in Ceylon",
+  },
+  {
+    id: 4,
+    title: "Flare Up Clothing – Digital Marketing Campaign",
+    description:
+      "A bold and trend-driven digital marketing campaign created to boost brand visibility and sales for Flare Up Clothing. Focused on youth fashion and streetwear culture, the strategy combined social media ads, influencer collaborations, and targeted content creation to amplify reach, drive engagement, and elevate the brand’s online identity.",
+    image:
+      "https://www.pngitem.com/pimgs/m/510-5109369_digital-marketing-company-digital-marketing-banner-png-transparent.png",
+    category: "Digital Marketing",
+    tags: ["Facebook Ads", "Instagram", "Analytics", "Automation"],
+    demoUrl: "https://www.facebook.com/profile.php?id=61569447028349",
+
+    date: "2024-10-05",
+    client: "Flare Up Clothing",
+  },
+
   // SEO Development
   // {
   //   id: 3,
@@ -134,22 +289,6 @@ const projects = [
   //   githubUrl: "https://github.com/example/seo-dashboard",
   //   date: "2024-01-10",
   //   client: "Digital Agency",
-  // },
-
-  // Digital Marketing
-  // {
-  //   id: 4,
-  //   title: "Social Media Campaign",
-  //   description:
-  //     "Multi-platform digital marketing campaign with automated posting and engagement tracking.",
-  //   image:
-  //     "https://images.unsplash.com/photo-1611162617474-5b21e879e113?w=600&h=400&fit=crop",
-  //   category: "Digital Marketing",
-  //   tags: ["Facebook Ads", "Instagram", "Analytics", "Automation"],
-  //   demoUrl: "https://demo-campaign.com",
-  //   githubUrl: "#",
-  //   date: "2024-03-01",
-  //   client: "Fashion Brand",
   // },
 
   // Email Marketing
@@ -216,22 +355,6 @@ const projects = [
   //   client: "Startup Company",
   // },
 
-  // Photography
-  // {
-  //   id: 9,
-  //   title: "Product Photography Portfolio",
-  //   description:
-  //     "Professional product photography and videography for e-commerce and marketing campaigns.",
-  //   image:
-  //     "https://images.unsplash.com/photo-1606983340126-99ab4feaa64a?w=600&h=400&fit=crop",
-  //   category: "Photography and Videography",
-  //   tags: ["Photography", "Product Shots", "Editing", "Commercial"],
-  //   demoUrl: "https://demo-photography.com",
-  //   githubUrl: "#",
-  //   date: "2024-03-05",
-  //   client: "Retail Brand",
-  // },
-
   // Content Writing
   // {
   //   id: 10,
@@ -246,22 +369,6 @@ const projects = [
   //   githubUrl: "#",
   //   date: "2024-02-10",
   //   client: "Tech Blog",
-  // },
-
-  // AI Video Creation
-  // {
-  //   id: 11,
-  //   title: "AI-Generated Marketing Videos",
-  //   description:
-  //     "Automated video creation using AI tools for marketing campaigns and social media content.",
-  //   image:
-  //     "https://images.unsplash.com/photo-1536240478700-b869070f9279?w=600&h=400&fit=crop",
-  //   category: "AI Video Creations",
-  //   tags: ["AI", "Video Generation", "Marketing", "Automation"],
-  //   demoUrl: "https://demo-ai-video.com",
-  //   githubUrl: "#",
-  //   date: "2024-03-12",
-  //   client: "Marketing Agency",
   // },
 
   // Logo Design
@@ -537,7 +644,7 @@ const ProjectPage = () => {
                           <ExternalLink size={20} />
                         </motion.a>
                       )}
-                      {project.githubUrl !== "#" && (
+                      {project.githubUrl && project.githubUrl !== "#" && (
                         <motion.a
                           href={project.githubUrl}
                           target="_blank"
